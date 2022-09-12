fd4c0af4e06c3979f3b3eb1028cc1302f2945584
Morphyum <zion@c-schwabe.de>
2022-09-12 19:36:44 +0200

    Slash commands

--- TRBBLBot/Program.cs
+++ TRBBLBot/Program.cs
@@ -1,19 +1,11 @@
 ﻿using Discord;
 using Discord.WebSocket;
-using Newtonsoft.Json.Linq;
 using System;
-using System.CodeDom;
-using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Net;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-namespace PPBot
-{
-    public class Program
-    {
+namespace PPBot {
+    public class Program {
         private string welcomeText = "First things firsts, please make sure you check out the<#572155952078258228> and the pinned messages in <#473044083330514944>. These will hopefully let you work out if it's the league for you. We're just heading into S7 and sorting out leagues at the moment.  If you're all happy with the rules etc... then you can find the sign up sheet in both <#473733975509041163> and pinned in <#753677775591702590>." +
                     "\nAs well as the league we also run side competitions.The main one is the perpetual ladder which is great for finding random games.They're just for fun but do allow you to play more between league games. Help on joining the ladder can be found at <#479662400640122890>. " +
                     "\nOther than that just look around and get involved.We've got plenty of opportunity to chat outside of just scheduling games so please feel free to get involved.If you've got any questions feel free to ask but make sure you include the <@&473036877524369459> tag so we know the question is there." +
@@ -24,30 +16,52 @@ namespace PPBot
 
         public async Task MainAsync() {
             LoadWelcome();
+            client.Ready += Ready;
             client.Log += Log;
             client.UserJoined += UserJoined;
             client.MessageReceived += MessageReceived;
+            client.SlashCommandExecuted += SlashCommandExecuted;
             var token = Secret.token; // Remember to keep this private!
             await client.LoginAsync(TokenType.Bot, token);
             await client.StartAsync();
-
 
             // Block this task until the program is closed.
             await Task.Delay(-1);
         }
 
+        public async Task Ready() {
+            var globalCommand = new SlashCommandBuilder();
+            globalCommand.WithName("set-welcome");
+            globalCommand.WithDescription("Change the welcome message");
+            globalCommand.AddOption(new SlashCommandOptionBuilder()
+                .WithName("message")
+                .WithRequired(true)
+                .WithDescription("The new message").WithType(ApplicationCommandOptionType.String)
+            );
+            var command = globalCommand.Build();
+            await client.CreateGlobalApplicationCommandAsync(command);
+        }
         private async Task MessageReceived(SocketMessage arg) {
             //Normal command go here
 
             //Admin commands go here
-            if (isAdmin(arg.Author as IGuildUser)) {
-                if (arg.Content.StartsWith("!setwelcome ")) {
+            if(isAdmin(arg.Author as IGuildUser)) {
+                if(arg.Content.StartsWith("!setwelcome ")) {
                     welcomeText = arg.Content.Replace("!setwelcome ", "");
                     SaveWelcome();
                     await arg.Channel.SendMessageAsync(welcomeText);
 
                 }
             }
+        }
+
+        private async Task SlashCommandExecuted(SocketSlashCommand command) {
+            if(command.CommandName.Equals("set-welcome")) {
+                welcomeText = (string)command.Data.Options.First().Value;
+                SaveWelcome();
+                await command.RespondAsync(welcomeText);
+            }
+
         }
 
         private async Task UserJoined(SocketGuildUser arg) {
@@ -62,7 +76,7 @@ namespace PPBot
         }
 
         private bool isAdmin(IGuildUser user) {
-            if (user.RoleIds.Any(r => r == 473036877524369459)) {
+            if(user.RoleIds.Any(r => r == 473036877524369459)) {
                 return true;
             }
             return false;
